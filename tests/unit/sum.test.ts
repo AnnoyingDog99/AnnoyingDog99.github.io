27e7e5c782f3af26459f73dde546978020bc69fc
AnnoyingDog99 <jan.vlasman99@gmail.com>
2023-02-08 15:41:53 +0100

    formatting

--- tests/unit/sum.test.ts
+++ tests/unit/sum.test.ts
@@ -2,11 +2,11 @@ import { it, expect } from 'vitest';
 import { sum } from '../../src/utils/sum';
 
 it('adds two numbers', () => {
-	expect(sum(2, 2)).toBe(4);
-    expect(sum(2,2)).not.toBe(2);
-    expect(sum(2,2)).not.toBe(3);
-    expect(sum(3,3)).toBe(6);
-    expect(sum(3,3)).not.toBe(9);
-    expect(sum(3,3)).not.toBe(0);
+    expect(sum(2, 2)).toBe(4);
+    expect(sum(2, 2)).not.toBe(2);
+    expect(sum(2, 2)).not.toBe(3);
+    expect(sum(3, 3)).toBe(6);
+    expect(sum(3, 3)).not.toBe(9);
+    expect(sum(3, 3)).not.toBe(0);
 
 });
